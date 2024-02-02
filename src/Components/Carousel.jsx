a03fc31cdfa918243f157c8d848bca81ef02ec76
demzytech <demiladeamodu@gmail.com>
2024-02-02 19:37:02 +0100

    added demzy-app

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -33,7 +33,7 @@ function Dark() {
         <Carousel.Caption>
         <div className='game1'>
             <div>
-              <img className='Logo1' src="https://assets-prd.ignimgs.com/2023/02/16/apexrevelry-1676588335122.jpg" alt="" />
+              <img className='Logo1' src="https://i.redd.it/5p16231zzx651.png" alt="" />
             </div>
             <div>
             <h5 className='we'>Apex Legend - The next evolution of Hero Shooter</h5>
