a984f4cbcaafa48d4160a31f1fed6fb668ee12e7
demzytech <demiladeamodu@gmail.com>
2024-02-04 23:26:42 +0100

    adding text and image contents

--- src/Components/pages/Signup.jsx
+++ src/Components/pages/Signup.jsx
@@ -73,26 +73,26 @@ function BasicExample() {
       <div>
       <Form.Group className="mb-3" controlId="formBasicFirstname">
         <Form.Label className='labelfor' style={{marginLeft: '300px'}}>First Name</Form.Label>
-        <Form.Control type="Firstname" placeholder="Enter Firstname" name='Firstname' value={formValues.Firstname} className='labelform' style={{marginLeft: '300px'}} onChange={handleChange} />
+        <Form.Control type="Firstname" placeholder="Enter Firstname" name='Firstname' value={formValues.Firstname} id='labelform' style={{marginLeft: '300px'}} onChange={handleChange} />
         
       </Form.Group>
       <p className='error'>{formErrors.Firstname}</p>
       <Form.Group className="mb-3" controlId="formBasicSurname">
         <Form.Label className='labelfor'style={{marginLeft: '300px'}}>Surname</Form.Label>
-        <Form.Control type="Surname" placeholder="Enter Surname" name='Surname' value={formValues.Surname} className='labelform' style={{marginLeft: '300px'}} onChange={handleChange}/>
+        <Form.Control type="Surname" placeholder="Enter Surname" name='Surname' value={formValues.Surname} id='labelform' style={{marginLeft: '300px'}} onChange={handleChange}/>
       </Form.Group>
       </div>
       <p className='error'>{formErrors.Surname}</p>
         
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label className='labelfor' style={{marginLeft: '300px'}}>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" name='email' value={formValues.email} className='labelform4' style={{marginLeft: '300px'}} onChange={handleChange}/>
+        <Form.Control type="email" placeholder="Enter email" name='email' value={formValues.email} id='labelform4' style={{marginLeft: '300px'}} onChange={handleChange}/>
       </Form.Group>
       <p className='error'>{formErrors.email}</p> 
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group>
         <Form.Label className='labelfor' style={{marginLeft: '300px'}}>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter new password" name='password' value={formValues.password} className='labelform4' style={{marginLeft: '300px'}} onChange={handleChange}/>
+        <Form.Control type="password" placeholder="Enter new password" name='password' value={formValues.password} id='labelform4' style={{marginLeft: '300px'}} onChange={handleChange}/>
       </Form.Group>
       <p className='error'>{formErrors.password}</p>
       
