c4bb9015ba7574fc4b5024fa28e2cd39e5998abf
demzytech <demiladeamodu@gmail.com>
2024-02-04 02:25:25 +0100

    adding new commit

--- src/Components/Cardy.jsx
+++ src/Components/Cardy.jsx
@@ -3,14 +3,19 @@ import Card from 'react-bootstrap/Card';
 
 function Cardy({imgUrl,  title}) {
   return (
-    <Card  style={{ width: '20rem', }} className='re'>
+    <div>
+      <Card  style={{ width: '20rem', }} className='re'>
       <Card.Img variant="top" className='img99' src={imgUrl}  />
       <Card.Body>
         <Card.Title className='tit'>
             {title}
         </Card.Title>
       </Card.Body>
+      <div>
+        <h1>demzyAmor</h1>
+      </div>
     </Card>
+    </div>
   );
 }
 
